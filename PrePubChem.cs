2b530d36122527a6c6059318223765c63a1e4fc7
KuebV <42746052+KuebV@users.noreply.github.com>
2021-09-23 00:12:26 -0700

    Update PrePubChem.cs

--- PrePubChem.cs
+++ PrePubChem.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Biotransformer
@@ -7,12 +8,19 @@ namespace Biotransformer
     public class PrePubChem
     {
         /// <summary>
-        /// Must be given individual compounds
+        /// Removes any additional text or lettering that comes after the compound that PubChem cannot handle
         /// </summary>
         /// <param name="Compound"></param>
-        /// <returns></returns>
+        /// <returns>Parsed Compound</returns>
         public static string ParseCompounds(string Compound)
         {
+            if (Compound.Contains("Esi+"))
+            {
+                int indexOfPlus = Compound.IndexOf("Esi+");
+                if (indexOfPlus >= 0)
+                    Compound = Compound.Substring(0, indexOfPlus);
+            }
+
             if (Compound.Contains('+'))
             {
                 int indexOfPlus = Compound.IndexOf('+');
@@ -22,5 +30,26 @@ namespace Biotransformer
 
             return Compound;
         }
+        public static void SeralizeFiles()
+        {
+            string pubChemOutput = Path.Combine(Directory.GetCurrentDirectory(), "PubChemOutput.txt");
+            string input = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+            if (!File.Exists(pubChemOutput))
+            {
+                Log.Error("PubChemOutput.txt does not exist, you must setup the program first!");
+                Log.Error("Press \'Enter\' to exit the program");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            if (!File.Exists(input))
+            {
+                Log.Error("input.txt does not exist, you must setup the program first!");
+                Log.Error("Press \'Enter\' to exit the program");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
     }
 }
